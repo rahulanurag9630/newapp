ab7203b9e05490e060de87bbb2bc9fa5f8268985
Anurag singh kushwaha <rahulanurag@gmail.com>
2023-08-04 23:59:08 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 
 import './App.css';
+import { BrowserRouter as Router,Routes,  Route} from "react-router-dom";
+
+
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import { About } from './components/About';
 
 function App() {
   return (
-    <div className="App">
-     <h1>this is i notebooke</h1>
-    </div>
+    <>
+     
+
+      <Router>
+      <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home></Home>}>
+            
+          </Route>
+          <Route exact path="/about"  element={<About />}>
+           
+          </Route>
+        </Routes>
+      </Router>
+
+    </>
   );
 }
 
